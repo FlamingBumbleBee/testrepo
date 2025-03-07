56c58cbe345fc37b712ded79aaa0939cfd5ce283
FlamingBumbleBee <matt.frerichs@gmail.com>
2025-03-07 12:50:51 -0500

    updating control_flow.js

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -43,4 +43,10 @@ switch (userType) {
         userCategory = "Unknown";
 }
 
-console.log("User Category:", userCategory)
+console.log("User Category:", userCategory);
+
+let isAuthenticated = true;
+
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not Authenticated"
+
+console.log("Authentication Status", authenticationStatus);
