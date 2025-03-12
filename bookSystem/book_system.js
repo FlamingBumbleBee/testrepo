4c1e7e966472501277e8a74eb4a7578b9cc48fcd
FlamingBumbleBee <matt.frerichs@gmail.com>
2025-03-12 15:30:04 -0400

    added delete buttion - not working yet

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -21,11 +21,12 @@ function addBook() {
 }
 
 function showbooks() {
-    const booksDiv = books.map((book, index) => `<h1>book Number: ${index + 1}</h1>
+    const booksDiv = books.map((book, index) => `<h1>Book Number: ${index + 1}</h1>
         <p><strong>Book Name: </strong>${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
+        <button onclick="deleteBook(index)">Delete</button>`
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -36,3 +37,9 @@ function showbooks() {
             document.getElementById('bookDescription').value = '';
             document.getElementById('pagesNumber').value = '';
  }
+
+function deleteBook(bookID) {
+    books.splice(bookID, 1);
+    console.log(books)
+    showbooks();
+}
